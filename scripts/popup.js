b7e9ee8fb62ac99169f0598e35ab6c9a15048563
Offerel <Offerel@users.noreply.github.com>
2019-01-12 21:46:09 +0100

    New Chrome migration test

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,17 +1,20 @@
-var background_page = browser.extension.getBackgroundPage();
-
-window.onload = function() {
+window.onload = function(background_page) {
 	localizeHtmlPage();
-	browser.storage.local.get().then( (option) => {
-		let last_message = option.last_message || browser.i18n.getMessage("popupNoSync");
+	
+	chrome.storage.local.get(null, function(options) {
+		let last_message = options['last_message'] || chrome.i18n.getMessage("popupNoSync");
 		document.getElementById("popupMessage").appendChild(document.createTextNode(last_message));
 	});
 	
 	document.getElementById("export").addEventListener("click", manualExport);
 	document.getElementById("import").addEventListener("click", manualImport);
 	document.getElementById("remove").addEventListener("click", manualRemove);
-	document.getElementById("settings").addEventListener("click", background_page.openSettings);
+	document.getElementById("settings").addEventListener("click", openSettings);
 };
+
+function openSettings() {
+	chrome.runtime.openOptionsPage();
+}
 
 function localizeHtmlPage() {
     var objects = document.getElementsByTagName('html');
@@ -22,7 +25,7 @@ function localizeHtmlPage() {
         var valStrH = obj.innerHTML.toString();
         var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function(match, v1)
         {
-            return v1 ? browser.i18n.getMessage(v1) : "";
+            return v1 ? chrome.i18n.getMessage(v1) : "";
         });
 
         if(valNewH != valStrH)
@@ -33,33 +36,36 @@ function localizeHtmlPage() {
 }
 
 function manualRemove() {
-	if(confirm(browser.i18n.getMessage("optionsBTNRemoveHint"))) {
+	var background_page = chrome.extension.getBackgroundPage();
+	if(confirm(chrome.i18n.getMessage("optionsBTNRemoveHint"))) {
 		background_page.removeAllMarks();
-		browser.storage.local.set({last_s: 1});
+		chrome.storage.local.set({last_s: 1});
 	}
 }
 
 function manualImport() {
-	if(confirm(browser.i18n.getMessage("popupImportConfirm"))) {
-		background_page.checkSettings();
-		
-		if(background_page.s_type == 'PHP') {
-			background_page.getAllPHPMarks();
-		}
-		else if(background_page.s_type == 'WebDAV') {
-			background_page.getDAVMarks();
-		}
+	var background_page = chrome.extension.getBackgroundPage();
+	if(confirm(chrome.i18n.getMessage("popupImportConfirm"))) {
+		chrome.storage.local.get(null, function(options) {
+			if(options['s_type'] == 'PHP') {
+				background_page.getAllPHPMarks();
+			}
+			else if(options['s_type'] == 'WebDAV') {
+				background_page.getDAVMarks();
+			}
+		});
 	}
 }
 
 function manualExport() {
-	background_page.checkSettings();
-
-	if(background_page.s_type == 'PHP') {
-		background_page.exportPHPMarks();
-	}
-	else if(background_page.s_type == 'WebDAV') {
-		background_page.saveAllMarks();
-	}
+	var background_page = chrome.extension.getBackgroundPage();
+	chrome.storage.local.get(null, function(options) {
+		if(options['s_type'] == 'PHP') {
+			background_page.exportPHPMarks();
+		}
+		else if(options['s_type'] == 'WebDAV') {
+			background_page.saveAllMarks();
+		}
+	});
 }
 
